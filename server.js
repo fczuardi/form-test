2e9411fed8bbcefb4f7f40b7f27f884a83531e8e
Fabricio C Zuardi <fabricio@fabricio.org>
2015-02-03 10:51:45 -0200

    adds a simple cookie-based session management for the backend reference

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var koa = require('koa'),
     serve = require('koa-static'),
     router = require('koa-router'),
-    bodyParser = require('koa-bodyparser');
+    bodyParser = require('koa-bodyparser'),
+    session = require('koa-session');
 
 var app = koa();
 var root = './dist/www',
     opts = {};
 
+app.keys = ['some secret hurr hurr hurr'];
 app.use(bodyParser());
+app.use(session({key: 'ASP.NET_SessionID'}, app));
 app.use(router(app));
 app.use(serve(root, opts));
 
 app.post('/Home/Login', function *(next) {
     var body = this.request.body,
         success = (body.email === 'foo' && body.password === 'bar');
+    this.session.signed = success;
     this.response.body = (success) ?
                         '{"Success":"true"}' :
                         '{"Success":"false"}';
@@ -21,7 +25,13 @@ app.post('/Home/Login', function *(next) {
 });
 
 app.get('/Home/Logout', function *(next) {
+    this.session = null;
     this.response.body = '{"Signed":"false"}';
+    yield next;
+});
+
+app.get('/Home/Admin', function *(next) {
+    this.response.body = '{"Signed":"'+this.session.signed+'"}';
     yield next;
 });
 
